a814cbe0d8f4caa848d89123362bf63dda52cf5b
issm <issmxx@gmail.com>
2011-08-14 21:13:22 +0900

    temporary prototyping.

--- lib/Nagoyapm/Amon2Test/Web/Dispatcher.pm
+++ lib/Nagoyapm/Amon2Test/Web/Dispatcher.pm
@@ -2,10 +2,68 @@ package Nagoyapm::Amon2Test::Web::Dispatcher;
 use strict;
 use warnings;
 use Amon2::Web::Dispatcher::Lite;
+use Nagoyapm::Amon2Test::Util qw/new_oid/;
+use Time::Piece;
+use Data::Dumper;
 
-any '/' => sub {
+get '/' => sub {
     my ($c) = @_;
-    $c->render('index.tx');
+    my ($req, $db) = ($c->req, $c->db);
+
+
+    my $itr_line = $db->line->find->sort({ created_at => -1, id => -1 })->limit(20);
+
+
+    my $vars = +{
+        lines => $itr_line,
+    };
+
+    $c->render('index.tx', $vars);
+};
+
+post '/' => sub {
+    my ($c) = @_;
+    my ($req, $db) = ($c->req, $c->db);
+
+    my $id_ins;
+    my $o = $db->line->query({}, { id => 0 })->sort({ id => -1 })->limit(1)->next;
+    $id_ins = ( $o->{id} || 0 ) + 1;
+
+
+    my $code = $req->param('code');
+
+    my $data = +{
+        id         => $id_ins,
+        code       => $code,
+        created_at => localtime->epoch,
+    };
+
+    my $ins = $db->line->insert($data);
+
+    $c->redirect('/');
+    #$c->render('index.tx');
+};
+
+
+get '/line/{id}' => sub {
+    my ($c, $args) = @_;
+    my ($req, $db) = ($c->req, $c->db);
+
+    my $line_id = $args->{id};
+    my $data;
+
+    my $oid = new_oid($line_id);
+
+    $data = $db->line->find_one({ _id => $oid });
+
+
+    my $vars = +{
+        uri  => $req->uri,
+        data => $data,
+    };
+
+
+    $c->render('line/show.tx', $vars);
 };
 
 
