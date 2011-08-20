3cf4b2668f0f8622105f3da2baa8e840d08d9949
issm <issmxx@gmail.com>
2011-08-20 15:51:28 +0900

    modified '/': lleval is available in each line.

--- static/js/page/index.js
+++ static/js/page/index.js
@@ -1,20 +1,22 @@
 $(function () {
     $('#lines table tbody td.eval a').click(function (ev) {
-        var code = $(this).parents('tr').find('td.line code').text();
+        var $tr = $(this).parents('tr');
+        var code = $tr.find('td.line code').text();
 
         lleval(code, null, function (data) {
             // ok
             if ( typeof data.error == 'undefined' ) {
-                // if ( data.stderr != '' ) {
-                //     alert( data.stderr );
-                // }
+                if ( data.stderr != '' ) {
+                    $tr.find('.line .lleval-stderr').text(data.stderr).slideDown();
+                }
 
-                // if ( data.stdout != '' ) {
-                //     alert(data.stdout);
-                // }
+                if ( data.stdout != '' ) {
+                    $tr.find('.line .lleval-stdout').text(data.stdout).slideDown();
+                }
             }
             // error
             else {
+                $tr.find('.line .lleval-stderr').text('何かエラーがおきました．．．').slideDown();
             }
 
         });
